856c31dd2cd25511320e5d78b79e5b0f4f97faa1
Roman Volkov <90625147+ChinaRBM@users.noreply.github.com>
2021-10-25 16:06:31 +0500

    Add files via upload

---
+++ lab2.pas
@@ -0,0 +1,17 @@
+﻿var x,a,i: integer;
+begin
+  x:=random(1,100);
+  writeln(x);
+  writeln('Отгадайте число за 6 раз');
+  writeln('Введите число');
+  for i:=1 to 6 do begin
+  readln(a);
+  if x=a then begin  
+  writeln('Угадал');
+  break;
+  end;
+    if a>x then writeln('Меньше')
+    else writeln('Больше')
+  end;
+  if (i=6)and(a<>x) then writeln('Не угадал');
+end.
