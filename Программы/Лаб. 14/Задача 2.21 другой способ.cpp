7afb83cf5af610fe2db82a37f3c752bc96bc6d43
Roman Volkov <90625147+ChinaRBM@users.noreply.github.com>
2021-12-21 10:20:57 +0500

    Add files via upload

---
+++ Программы/Лаб. 14/Задача 2.21 другой способ.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, n1, n2;
+    cin >> n;
+    cout << n << ' ';
+    n1 = n;
+    while (n != 0)
+    {
+        cin >> n;
+        cout << n << ' ';
+        n2 = n;
+        if (n2 < n1 && n2!=0)
+        {
+            cout << "Not sequentional";
+            break;
+        }
+        n1 = n2;
+    }
+}
