90731fe205f42d92c5f352d50a53bdcbf9b4c9f4
Roman Volkov <90625147+ChinaRBM@users.noreply.github.com>
2021-12-21 12:55:42 +0500

    Add files via upload

---
+++ Программы/Лаб. 16/Задача 3.8.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n, t;
+    n = 6;
+    int a[6];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < n / 2; i++)
+    {
+        t = a[i];
+        a[i] = a[n - i - 1];
+        a[n - i - 1] = t;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
