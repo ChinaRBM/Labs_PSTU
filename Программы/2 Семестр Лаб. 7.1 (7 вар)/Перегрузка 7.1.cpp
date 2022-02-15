6fc007c901b6c8ad5e622bdfd3e4ed0df7c9be76
Roman Volkov <90625147+ChinaRBM@users.noreply.github.com>
2022-02-15 22:48:32 +0500

    Add files via upload

---
+++ Программы/2 Семестр Лаб. 7.1 (7 вар)/Перегрузка 7.1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <complex>
+using namespace std;
+double delenie(int a, int b)
+{
+	cout << a / b << endl;
+	return 0;
+}
+double delenie(complex<double> a, complex<double> b)
+{
+	cout << a / b << endl;
+	return 0;
+}
+int main()
+{
+	double m1 = delenie(23, 8);
+	double m2 = delenie((16, 2), (5, 2));
+}
