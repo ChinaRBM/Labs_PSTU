06619fe22dffdaf73a38845fd53fd4cc1c9aebdd
Roman Volkov <90625147+ChinaRBM@users.noreply.github.com>
2022-02-21 15:21:31 +0500

    Add files via upload

---
+++ Программы/2 Семестр Лаб. 6 (7 вар)/Лаб. 6.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	string sort = "";
+	cin >> sort;
+	for (int i = 0; i < sort.length() - 1; i++)
+	{
+		for (int j = i + 1; j < sort.length(); j++)
+		{
+			if (sort[i] > sort[j])
+			{
+				char tmp = sort[i];
+				sort[i] = sort[j];
+				sort[j] = tmp;
+			}
+		}
+	}
+	cout << sort;
+}
