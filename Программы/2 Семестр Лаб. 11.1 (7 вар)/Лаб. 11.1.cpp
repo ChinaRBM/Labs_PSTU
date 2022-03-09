de22cabb83ce817abf1d647bbcbdf9b0a597c8b2
Roman Volkov <90625147+ChinaRBM@users.noreply.github.com>
2022-03-09 17:55:19 +0500

    Add files via upload

---
+++ Программы/2 Семестр Лаб. 11.1 (7 вар)/Лаб. 11.1.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct point
+{
+	double data;
+	point* next;
+};
+
+point* make_list(int n)
+{
+	point* beg;
+	point* p, * r;
+	beg = new(point);
+	cin >> beg->data;
+	beg->next = 0;
+	p = beg;
+	for (int i = 0; i < n - 1; i++)
+	{
+		r = new(point);
+		cin >> r->data;
+		r->next = 0;
+		p->next = r;
+		p = r;
+	}
+	return beg;
+}
+
+void print_list(point* beg)
+{
+	point* p = beg;
+	while (p != nullptr)
+	{
+		cout << p->data << "\t";
+		p = p->next;
+	}
+}
+
+point* add_point(point* beg, int n)
+{
+	point* p = beg;
+	point* New = new(point);
+    cin >> New->data;
+	if (p->data < 0)
+	{
+		New->next = beg;
+		beg = New;
+		return beg;
+	}
+	for (int i = 0;i < n && p != 0; i++)
+	{
+		p = p->next;
+		if (p != nullptr && p->data < 0)
+		{
+			New->next = p->next;
+			p->next = New;
+		}
+	}
+	return beg;
+}
+
+int main()
+{
+	int n = 10;
+	point* beg = make_list(n);
+	add_point(beg,n);
+	print_list(beg);
+}
