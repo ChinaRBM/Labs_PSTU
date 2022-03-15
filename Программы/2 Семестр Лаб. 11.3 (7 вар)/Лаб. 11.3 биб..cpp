0348142ba2041c74e79625c44a2bc81123e68eac
Roman Volkov <90625147+ChinaRBM@users.noreply.github.com>
2022-03-15 21:01:33 +0500

    Add files via upload

---
+++ Программы/2 Семестр Лаб. 11.3 (7 вар)/Лаб. 11.3 биб..cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	double k, j;
+	cin >> n;
+	j = n;
+	stack <double> r;
+	for (int i = 0; i < n; i++)
+	{
+		k = rand() % 200 - 100;
+		if (k < 0)
+		{
+			r.push(0);
+			r.push(k);
+			j++;
+		}
+		else
+		{
+			r.push(k);
+		}
+	}
+	for (int i = 0; i < j; i++)
+	{
+		cout << r.top() << " ";
+		r.pop();
+	}
+}
