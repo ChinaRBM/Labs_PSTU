e14dddf356e73aaeeb2239d450f81743f2449993
Roman Volkov <90625147+ChinaRBM@users.noreply.github.com>
2022-03-15 20:19:32 +0500

    Add files via upload

---
+++ Программы/2 Семестр Лаб. 11.2 (7 вар)/Лаб 11.2.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct point
+{
+	int data;
+	point* next;
+	point* prev;
+};
+
+point* make_list(int n)
+{
+	point* beg;
+	point* p, * r;
+	beg = new(point);
+	cin >> beg->data;
+	beg->next = 0;
+	p = beg;
+	for (int i = 0; i < n - 1; i++)
+	{
+		r = new(point);
+		cin >> r->data;
+		r->next = 0;
+		p->next = r;
+		p = r;
+	}
+	return beg;
+}
+
+void print_list(point* beg)
+{
+	point* p = beg;
+	while (p != nullptr)
+	{
+		cout << p->data << "  ";
+		p = p->next;
+	}
+	cout << endl;
+}
+
+point* del_point(point* beg, int n)
+{
+	bool fl = false;
+	point* p = beg;
+	if ((p->data) % 2 == 0 && fl == false)
+	{
+		beg = beg->next;
+		delete p;
+		fl = true;
+		return beg;
+	}
+	for (int i = 0; i < n && p != 0; i++)
+	{
+		p = p->next;
+		if (p != nullptr && (p->next->data)%2==0 && fl==false)
+		{
+			point* r = p->next;
+			p->next = r->next;
+			delete r;
+			fl = true;
+			return beg;
+		}
+	}
+}
+
+int main()
+{
+	int n = 10;
+	point* beg = make_list(n);
+	del_point(beg, n);
+	print_list(beg);
+}
