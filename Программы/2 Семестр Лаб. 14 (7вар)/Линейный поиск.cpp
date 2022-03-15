50c079e82445027d7ae5fa2435f9f5d9f805b669
Roman Volkov <90625147+ChinaRBM@users.noreply.github.com>
2022-03-15 20:20:52 +0500

    Add files via upload

---
+++ Программы/2 Семестр Лаб. 14 (7вар)/Линейный поиск.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int fill(int k,int *n)
+{
+    for (int i = 0; i < k; i++)
+    {
+        n[i] = rand() % 10 + 1;
+    }
+    return 0;
+}
+
+int print(int k, int* n)
+{
+    for (int i = 0; i < k; i++)
+    {
+        cout << n[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
+
+int main()
+{
+    bool fl=false;
+    int k, p;
+    int count=0;
+    cin >> k;
+    int *n = new int[k];
+    fill(k, n);
+    cout << "Enter N: ";
+    cin >> p;
+    for (int i = 0; i < k; i++)
+    {
+        if (fl==false)
+        {
+            count++;
+        }
+        if ((n[i] == p) && (fl==false))
+        {
+            n[i] = p * 2;
+            fl = true;
+        }
+    }
+
+    if (fl == false)
+    {
+        cout << "There is no N";
+    }
+    else
+    {
+        print(k, n);
+        cout << count;
+    }
+}
