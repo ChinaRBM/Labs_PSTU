3345168fc9b072b6a90ce3e202b9baba8412e5c4
Roman Volkov <90625147+ChinaRBM@users.noreply.github.com>
2022-03-16 11:49:15 +0500

    Add files via upload

---
+++ Программы/2 Семестр Лаб. 11.4 (7 вар)/Лаб. 11.4 биб..cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    queue <int> r;
+    bool fl=false;
+    int n, k;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        k = rand() % 200 - 100;
+        r.push(k);
+        if ((k % 2 == 0) && (fl == false))
+        {
+            r.pop();
+            fl = true;
+        }
+    }
+    if (fl == true)
+    {
+        n--;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << r.back()<<" ";
+        r.pop();
+    }
+}
